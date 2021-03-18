a712fb901f9cc42ca98a19895dcf07ccf0d8d836
Han <45216932+Interval-0@users.noreply.github.com>
2021-03-18 21:48:03 +0900

    Update New-WindowsServer2019VM.ps1

--- New-WindowsServer2019VM.ps1
+++ New-WindowsServer2019VM.ps1
@@ -1,30 +1,27 @@
 [CmdletBinding()]
 param(
 
-    [Parameter(Mandatory = $true)]
-    [string]$Name,
+    [string]$uid,
 
-    [Parameter(Mandatory = $true)]
     [int64]$CPU,
 
-    [Parameter(Mandatory = $true)]
     [int64]$Memory,
 
-    [Parameter(Mandatory = $true)]
     [uint64]$Storage,
 
-    [Parameter(Mandatory = $true)]
-    [ValidateSet('External Switch', 'Internal Switch', 'Private Switch')]
+    [ValidateSet('Ext1', 'Internal Switch', 'Private Switch')]
     [string]$SwitchName,
 
-    [Parameter(Mandatory = $true)]
     [string]$WindowsISO,
 
-    [Parameter(Mandatory = $true)]
     [ValidateSet('Windows Server 2019 Datacenter Evaluation', 'Windows Server 2019 Datacenter Evaluation (Desktop Experience)')]
     [string]$WindowsEdition,
 
-    [string]$AdminPassword
+    [string]$AdminPassword,
+
+    [string]$DstDrive,
+    
+    [string]$uid
 
 )
 
@@ -45,23 +42,23 @@ $WindowsAMVA = 'H3RNG-8C32Q-Q8FRX-6TDXV-WMBMW'
 
 
 ## Quick Check for inflight and duplicate builds
-if (Test-Path -Path "C:\Temp\HVA\$Name") {
-    $Name = $Name + $BuildID
+if (Test-Path -Path "C:\Temp\HVA\$uid") {
+    $uid = $uid + $BuildID
 }
-elseif (Get-VM | Where-Object Name -EQ $Name) {
-    $Name = $Name + $BuildID
+elseif (Get-VM | Where-Object Name -EQ $uid) {
+    $uid = $uid + $BuildID
 }
 
 ## Trim and clean name
-$Name = $Name -replace '\s', '' -replace '^(.{0,14}).*', '$1'
+$uid = $uid -replace '\s', '' -replace '^(.{0,14}).*', '$1'
 
 ## Setup Working Directories for build
-$TempDirectory = "C:\Temp\HVA\$Name"
-$UnattendDirectory = "C:\Temp\HVA\$Name\Unattend"
+$TempDirectory = "D:\Temp\HVA\$uid"
+$UnattendDirectory = "$DstDrive\Temp\HVA\$uid\Unattend"
 New-Item -Path $TempDirectory -ItemType Directory -Force | Out-Null
 New-Item -Path $UnattendDirectory -ItemType Directory -Force | Out-Null
 ## Set VHD Path
-$VHDPath = (Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData).DefaultVirtualHardDiskPath + "\$Name.vhdx"
+$VHDPath = (Get-WmiObject -Namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData).DefaultVirtualHardDiskPath + "\$uid.vhdx"
 
 # User Details
 ## Administrator
@@ -91,9 +88,9 @@ $UnattendTemplate = @'
             <ComputerName></ComputerName>
         </component>
         <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <InputLocale>en-US</InputLocale>
-            <SystemLocale>en-US</SystemLocale>
-            <UserLocale>en-US</UserLocale>
+            <InputLocale>ko-KR</InputLocale>
+            <SystemLocale>ko-KR</SystemLocale>
+            <UserLocale>ko-KR</UserLocale>
         </component>
         <component name="Microsoft-Windows-Security-SPP-UX" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <SkipAutoActivation>true</SkipAutoActivation>
@@ -137,7 +134,7 @@ $xml = [xml]$UnattendTemplate
 ## Answer destination
 $UnattendPath = "$UnattendDirectory\unattend.xml"
 ## Define unattended settings
-$xml.unattend.settings[0].component[0].ComputerName = $Name
+$xml.unattend.settings[0].component[0].ComputerName = $uid
 $xml.unattend.settings[0].component[0].ProductKey = $WindowsKMS
 $xml.unattend.settings[1].component.UserAccounts.AdministratorPassword.Value = $AdminPassword
 ## Save and Write answers
@@ -147,7 +144,7 @@ $xml.Save($writer)
 $writer.Dispose()
 
 ## Create VHDX from ISO
-Write-Host "[$BuildID] - $(Get-date) - Mixing $Name with $WindowsEdition" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Mixing $uid with $WindowsEdition" -ForegroundColor Yellow
 
 ## Import Convert-WindowsImage.ps1 function for use
 . .\Convert-WindowsImage.ps1
@@ -156,7 +153,7 @@ Write-Host "[$BuildID] - $(Get-date) - Mixing $Name with $WindowsEdition" -Foreg
 Convert-WindowsImage -SourcePath $WindowsISO -Edition $WindowsEdition -TempDirectory $TempDirectory -UnattendPath $UnattendPath -SizeBytes $Storage -DiskLayout UEFI -VHDPath $VHDPath -VHDFormat VHDX
 
 ## Setup new VM
-$SetupVM = New-VM -Name $Name -Generation 2 -MemoryStartupBytes $Memory -VHDPath $VHDPath -SwitchName $SwitchName
+$SetupVM = New-VM -Name $uid -Generation 2 -MemoryStartupBytes $Memory -VHDPath $VHDPath -SwitchName $SwitchName
 $SetupVM | Set-VMProcessor -Count $CPU
 $SetupVM | Get-VMIntegrationService -Name "Guest Service Interface" | Enable-VMIntegrationService -Passthru
 $SetupVM | Set-VMMemory -DynamicMemoryEnabled $true
@@ -164,33 +161,33 @@ $SetupVM | Set-VM -AutomaticCheckpointsEnabled $false
 $SetupVM | Start-VM
 
 ## Wait for installation complete
-Wait-VM -Name $Name -For Heartbeat
+Wait-VM -Name $uid -For Heartbeat
 
 ## START CONFIG ##
 
 do {
     Start-Sleep -Seconds 60
-    $StartSession = New-PSSession -VMName $Name -Credential $Credential -ErrorAction SilentlyContinue
+    $StartSession = New-PSSession -VMName $uid -Credential $Credential -ErrorAction SilentlyContinue
 } until ($StartSession.State -eq "Opened")
 
 ## Connect to New VM and enable Remote Management
-Write-Host "[$BuildID] - $(Get-date) - Setting up $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Setting up $uid" -ForegroundColor Yellow
 
 ## Setup Ansible for Host Management
-Write-Host "[$BuildID] - $(Get-date) - Enabling Ansible Management on $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Enabling Ansible Management on $uid" -ForegroundColor Yellow
 Invoke-Command -Session $StartSession -ScriptBlock {
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
 }
  
 ## Install Chocolatey
-Write-Host "[$BuildID] - $(Get-date) - Installing Chocolatey on $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Installing Chocolatey on $uid" -ForegroundColor Yellow
 Invoke-Command -Session $StartSession -ScriptBlock {
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 ## Setup RDP for Host Management if Desktop
 if ($WindowsEdition -match "Desktop") {
-    Write-Host "[$BuildID] - $(Get-date) - Enabling RDP on $Name" -ForegroundColor Yellow
+    Write-Host "[$BuildID] - $(Get-date) - Enabling RDP on $uid" -ForegroundColor Yellow
     Invoke-Command -Session $StartSession -ScriptBlock {
         Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
         Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
@@ -216,21 +213,21 @@ else {
 }
 
 ## Reboot
-Write-Host "[$BuildID] - $(Get-date) - Restarting $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Restarting $uid" -ForegroundColor Yellow
 Invoke-Command -Session $StartSession -ScriptBlock {
     shutdown /r
 }
 
 ## Create new session after reboot and continue
-Wait-VM -Name $Name -For Heartbeat
+Wait-VM -Name $uid -For Heartbeat
 do {
     Start-Sleep -Seconds 60
-    $StartSession2 = New-PSSession -VMName $Name -Credential $Credential -ErrorAction SilentlyContinue
+    $StartSession2 = New-PSSession -VMName $uid -Credential $Credential -ErrorAction SilentlyContinue
 } until ($StartSession2.State -eq "Opened")
 
 ## Set AVMA Key for Windows Server 2019
 ## https://docs.microsoft.com/en-us/windows-server/get-started-19/vm-activation-19
-Write-Host "[$BuildID] - $(Get-date) - Setting AVMA Key on $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Setting AVMA Key on $uid" -ForegroundColor Yellow
 Invoke-Command -Session $StartSession2 -ScriptBlock {
     cscript.exe $env:SystemRoot\System32\slmgr.vbs /ipk H3RNG-8C32Q-Q8FRX-6TDXV-WMBMW
     # Activate Windows
@@ -240,7 +237,7 @@ Invoke-Command -Session $StartSession2 -ScriptBlock {
 }
 
 ## Disable Realtime Antivirus monitoring, Clean install
-Write-Host "[$BuildID] - $(Get-date) - Cleaning up $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Cleaning up $uid" -ForegroundColor Yellow
 Invoke-Command -Session $StartSession2 -ScriptBlock {
     # Disable realtime monitoring
     Set-MpPreference -DisableRealtimeMonitoring $true
@@ -250,13 +247,13 @@ Invoke-Command -Session $StartSession2 -ScriptBlock {
 }
 
 ## Reboot
-Write-Host "[$BuildID] - $(Get-date) - Restarting $Name" -ForegroundColor Yellow
+Write-Host "[$BuildID] - $(Get-date) - Restarting $uid" -ForegroundColor Yellow
 Invoke-Command -Session $StartSession2 -ScriptBlock {
     shutdown /r
 }
 
 # Waiting for Heartbeat prior to finish
-Wait-VM -Name $Name -For Heartbeat
+Wait-VM -Name $uid -For Heartbeat
 
 # END CONFIG
 
@@ -267,7 +264,7 @@ $TotalTime = (New-TimeSpan -Start $StartTime -End $EndTime).Minutes
 # END BUILD
 
 ## Wrap it up
-Write-Host "[$BuildID] - $(Get-date) - $Name is now ready"
+Write-Host "[$BuildID] - $(Get-date) - $uid is now ready"
 Write-Host "[$BuildID] - $(Get-date) - Credentials: $UserName\$AdminPassword" -ForegroundColor Yellow
 Write-Host "[$BuildID] - $(Get-date) - Build End" -ForegroundColor Yellow
 Write-Host "[$BuildID] - $(Get-date) - Completed in $TotalTime Minutes" -ForegroundColor Green
